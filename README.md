f6967965ec05e0daeb0c9104f85984970730f393
ibrah-alt <mahamadouibrahim363@gmail.com>
2026-03-28 21:42:28 +0100

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Medical-Chatbot
+conda create -n medibot python=3.10 -y
+
+conda activate medibot
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setup import find_packages, setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Mahamadou Hima Ibrahim",
+    author_email="mahamadouibrahim363@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+
+)
